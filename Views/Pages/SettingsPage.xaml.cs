6c4e940497d4c325b696acdf336be4fb8fb6c3d6
caojiachen1 <caojiachen1@163.com>
2025-08-09 10:40:57 +0800

    refactor: Revamp SettingsPage layout for improved organization and readability

--- Views/Pages/SettingsPage.xaml.cs
+++ Views/Pages/SettingsPage.xaml.cs
@@ -17,16 +17,5 @@ namespace ProjectManager.Views.Pages
 
             InitializeComponent();
         }
-
-        private void ScrollViewer_PreviewMouseWheel(object sender, MouseWheelEventArgs e)
-        {
-            if (sender is ScrollViewer scrollViewer)
-            {
-                // 计算滚动量，调整滚动速度
-                double scrollAmount = e.Delta * 0.8;
-                scrollViewer.ScrollToVerticalOffset(scrollViewer.VerticalOffset - scrollAmount);
-                e.Handled = true;
-            }
-        }
     }
 }
